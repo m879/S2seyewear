31b7522b16c4de67f9676577b77b9591ba5d8811
Meraj <merajahmed2112@gmail.com>
2021-12-04 18:16:57 +0530

    category changes on home page

--- src/Components/Categories/Category.js
+++ src/Components/Categories/Category.js
@@ -1,24 +1,49 @@
 import React from 'react'
 import './Category.css' 
 import category from '../../images/category.png'
+import {Row,Col} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+
+const categoryName=[
+    {name:'Men eyewear'},
+    {name:'Women eyewear'},
+    {name:'Kids eyewear'},
+    {name:'Men sunglasses'},
+    {name:'Women sunglasses'},
+    {name:'Kids sunglasses'}
+]
 
 function Category() {
     return (
         <>
         <h2 className="green-heading">Categories</h2>
         <div className="category-container">
-            <div className="category">
+                <Row>
+                    {
+                        categoryName.map((row) => (
+                            <Col lg={4} xs={12}>
+                                <NavLink to='/eyewear' style={{textDecoration:'none'}}>
+                                <div className="category">
+                                    <img src={category} alt="men" />
+                                    <span>{row.name}</span>
+                                </div>
+                                </NavLink>
+                            </Col>
+                        ))
+                    }
+                </Row>
+            {/* <div className="category">
                 <img src={category} alt="men"/>
-                <span>Men</span>
+                <span>Women eyewear</span>
             </div>
             <div className="category">
                 <img src={category} alt="men"/>
-                <span>Women</span>
+                <span>Kids eyewear</span>
             </div>
             <div className="category">
                 <img src={category} alt="men"/>
-                <span>kids</span>
-            </div>
+                <span>Men sunglasses</span>
+            </div> */}
         </div>
         </>
     )
